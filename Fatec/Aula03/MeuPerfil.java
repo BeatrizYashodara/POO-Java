b04600dc3f5c48a4d62d2c0bf4244b102f60e892
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-03-03 16:13:39 -0300

    Create MeuPerfil.java

---
+++ Fatec/Aula03/MeuPerfil.java
@@ -0,0 +1,23 @@
+
+
+
+public class MeuPerfil {
+
+    public static void main (String [] args) {
+    String nome = "Beatriz";
+    int idade = 23;
+    double peso = 58.5;
+    boolean ehDoador = true;
+    double altura = 1.58;
+    boolean temEmprego = true;
+
+    System.out.println("---Meu Perfil---");
+    System.out.println("nome: "+ nome);
+    System.out.println("idade "+ idade);
+    System.out.println("peso "+ peso);
+    System.out.println("eh doadora? " + ehDoador);
+    System.out.println("altura " + altura);
+    System.out.println("tem emprego? " + temEmprego);
+    }
+
+}
