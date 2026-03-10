22f498dfdb17531aa32e2134e7f904ec33e6d4f3
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-03-10 14:00:57 -0300

    Create Pessoa.java

---
+++ Fatec/Aula04/Pessoa.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Pessoa {
+    String nome;
+    String cpf;
+    double altura;
+    double peso;
+    Pessoa mae;
+    Pessoa pai;
+    LocalDate nascimento;
+
+    int getIdade() { 
+        LocalDate dataAtual = LocalDate.now();
+        Period periodo = Period.between (nascimento, dataAtual);
+        return periodo.getYears();
+    }
+}
