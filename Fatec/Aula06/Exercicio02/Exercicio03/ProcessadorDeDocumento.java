af5fb1930e49ba0a2a6b7c24a676c5fbab6d29d7
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-03-24 16:17:42 -0300

    Create ProcessadorDeDocumento.java

---
+++ Fatec/Aula06/Exercicio02/Exercicio03/ProcessadorDeDocumento.java
@@ -0,0 +1,8 @@
+public class ProcessadorPDF extends ProcessadorDeDocumento {
+
+    @Override
+    public void formatar() {
+        System.out.println("Formatando conteúdo para PDF...");
+    }
+    
+}
