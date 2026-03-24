0c4b2161cd7e7f29bf0e420420a1f89371e4ed6f
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-03-24 16:18:13 -0300

    Create ProcessadorDOCX.java

---
+++ Fatec/Aula06/Exercicio02/Exercicio03/ProcessadorDOCX.java
@@ -0,0 +1,8 @@
+public class ProcessadorDOCX extends ProcessadorDeDocumento {
+
+    @Override
+    public void formatar() {
+        System.out.println("Formatando documento para DOCX...");
+    }
+    
+}
