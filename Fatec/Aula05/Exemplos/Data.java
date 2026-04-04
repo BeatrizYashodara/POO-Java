217fe5c3c70883e386978d9138454fbfb9dacb9f
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-04-04 18:12:32 -0300

    Implement constructors and getData method in Data class
    Added constructors and getData method to format date.

--- Fatec/Aula05/Exemplos/Data.java
+++ Fatec/Aula05/Exemplos/Data.java
@@ -3,10 +3,42 @@ public class Data {
 	private int mes;
 	private int ano;
 
+	public Data() {
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+	}
+
+	public Data( int dia, int mes, int ano ) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+	
 	public static String[] meses = { "JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ" };
 
 public String getNomeDoMes() {
 	return meses[this.mes-1];
 	}
 
+/*
+Tarefa 01: 
+No projeto da aula anterior Crie na classe Data um método chamado “getData” sem
+parâmetros que retorne uma String com a data no formato
+“DD/MM/AAAA”
+SOLUÇÃO: */
+	public String getData() {
+	String out = "";
+	if(getDia() <10) out += "0";
+	out += getDia() + "/";
+	if(getMes() < 10 ) out += "0";
+	out += getMes() + "/";
+	out += getAno();
+	return out;
+	}
+
+
+
+	
+	
 }
