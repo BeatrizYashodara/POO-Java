6d0b0e77f4a822fafeff5d8e7aa4605c730c3314
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-04-04 18:48:20 -0300

    Add Funcionario class with salary and bonus methods

---
+++ Fatec/Aula06/Exemplos/Polimorfismo/Funcionario.java
@@ -0,0 +1,14 @@
+public class Funcionario {
+  protected String nome;
+  protected double salario;
+
+  public Funcionario(String nome, double salario){
+    this.nome = nome;
+    this.salario = salario;
+  }
+
+  public double getBonificacao(){
+    return this.salario * 0.10;
+  }
+
+}
