440d52f07f9f9c2a880f961cd8f500d93a629812
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-04-04 18:44:40 -0300

    Fix class name and add constructor to Funcionario

---
+++ Fatec/Aula06/Exemplos/Herança/Funcionario.java
@@ -0,0 +1,10 @@
+public class Funcionario {
+  protected String nome;
+  protected double salario;
+
+  public Funcionario(String nome, double salario){
+    this.nome = nome;
+    this.salario = salario;
+  }
+  
+}
