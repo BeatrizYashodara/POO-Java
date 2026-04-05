42c8d1e5a879bebb1e4b26f9437a4774d062fa77
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-04-05 19:11:32 -0300

    Create Figura.java

---
+++ Fatec/Aula06/Exercicio01/Figura.java
@@ -0,0 +1,15 @@
+/*
+Classe Abstrata que representa o conceito genérico de uma figura geométrica.
+Não pode ser instanciada diretamente.
+Obriga todas as subclasses a implementarem o método de cálculo de área.
+*/
+
+public abstract class Figura {
+/*
+Método abstrato (sem implementação) que deve ser sobrescrito pelas subclasses 
+para calcular a área específica da figura.
+@return O valor da área calculada (double)
+*/
+public abstract double calcularArea();
+  
+}
