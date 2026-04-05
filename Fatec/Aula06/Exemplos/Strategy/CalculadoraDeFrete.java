7dd72d19b03d6aabbbede4387faa875b3a58c45e
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-04-05 19:20:38 -0300

    Create CalculadoraDeFrete.java

---
+++ Fatec/Aula06/Exemplos/Strategy/CalculadoraDeFrete.java
@@ -0,0 +1,17 @@
+/*
+Classe de Contexto que utiliza uma estratégia de frete para realizar o cálculo.
+Ela não conhece os detalhes de como o cálculo é feito, apenas delega a responsabilidade
+para o objeto de estratégia que recebe.
+*/
+
+public class CalculadoraDeFrete {
+/*
+Executa o cálculo de frete utilizando a estratégia fornecida.
+@param tipo O objeto da estratégia de frete a ser usada.
+@param distancia A distância para o cálculo.
+@param O valor do frete calculado pela estratégia.
+*/
+  public double calculaFrete (TipoDeFrete tipo, double distancia){
+    return tipo.calcular(distancia);
+  }
+}
