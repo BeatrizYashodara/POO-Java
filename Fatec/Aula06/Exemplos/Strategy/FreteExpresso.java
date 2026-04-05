52ff7f248f76cd23a7962b6494ecb8f0aefa6271
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-04-05 19:22:59 -0300

    Create FreteExpresso.java

---
+++ Fatec/Aula06/Exemplos/Strategy/FreteExpresso.java
@@ -0,0 +1,12 @@
+/*
+Estratégia Concreta para o cálculo de um frete expresso.
+*/
+
+public class FreteExpresso implements TipoDeFrete {
+
+  @Override
+  public double calcular(double distancia) {
+    // Lógica específica do frete expresso: R$2,50 por km.
+    return distancia * 2.50;
+  }
+}
